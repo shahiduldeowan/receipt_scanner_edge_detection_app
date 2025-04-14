146476b0adf7818b061d0874160b9526c2d84a31
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-14 14:07:08 +0600

    fix: Set minSdkVersion to 21 in build.gradle.kts for compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,9 +22,7 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.receipt_scanner_edge_detection_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
